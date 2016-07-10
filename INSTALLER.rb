d37446b63779c639b5e1656dbc8b4093db3cacdb
MichaelAzzara <michaelj.azzara@gmail.com>
2016-07-10 13:51:02 -0400

    Working to future goals
    setting up file for future modifications. Eventually the goal is to have Twonky create multiple instances of itself - TwonkyMovies, TwonkyMovies3D, TwonkyMusic.... or as many as the user requires. For now it just unpacks itself.

--- INSTALLER.rb
+++ INSTALLER.rb
@@ -2,15 +2,22 @@
 #
 # Tool we will be using for directory manipulation
 require 'fileutils'
-# why not..
-print "Prepairing Twonky install"
+#
+#key/values -- can modify values to create multiple instances of twonky (w.i.p) requires a lot more code and modification of ini.script ect...
+name = "TwonkyRaspbian"
+init = "twonkystartup"
+twonky_dir = "/home/twonky"
+#
+#
+# feedback
+puts "Prepairing Twonky install"
 #
 # 1. Move Twonky Starter into the /etc/init.d dir and make it executable
-FileUtils.install 'twonkystartup', '/etc/init.d', :mode => 0755, :verbose => true
+FileUtils.install '#{init}', '/etc/init.d', :mode => 0755, :verbose => true
 puts "1. Success"
 
 # 2. Set Twonky to load at boot
-system "update-rc.d /etc/init.d/twonkystartup defaults"
+system "update-rc.d /etc/init.d/#{init} defaults"
 puts "2. Success"
 
 # 3. Create user to run twonky
@@ -19,13 +26,12 @@ puts "3. Success"
 
 # 4. Make Twonky files Execuable and change folder permissions
 system "chmod 700 twonkys* cgi-bin/* plugins/*"
-system "chown twonky:pi /usr/share/TwonkyRaspbian -R"
+system "chown twonky:pi /usr/share/#{name} -R"
 puts "4. Success"
 
 # 5. Create appdata folder to host databases ect.
-twonky_dir = "/home/twonky"
 FileUtils.mkdir(twonky_dir) unless File.directory?(twonky_dir)
-system "chown twonky:pi /home/twonky -R"
+system "chown twonky:pi #{twonky_dir} -R"
 puts "5. Success"
 
 # 6. Install dependencies
